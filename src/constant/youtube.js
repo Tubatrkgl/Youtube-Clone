5a8f35f8b12812e5b1bd73d9bac266e87ff15fa1
Tuba Türkoğlu <92tuba92@gmail.com>
2024-04-16 15:56:59 +0300

    apı-key

--- src/constant/youtube.js
+++ src/constant/youtube.js
@@ -1,5 +1,5 @@
 
-export const API_KEY = "AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8";
+export const API_KEY = "AIzaSyDHOMpLGwqnnIxcsjJYvCwskx5wEM5ULEE";
 export const YOUTUBE_VIDEO_API = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=TR&key=${API_KEY}`;
 
 export const BASE_URL = "https://www.googleapis.com/youtube/v3";
